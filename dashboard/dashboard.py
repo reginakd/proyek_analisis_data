c371b1ecce07b7defd6dbbedc99d4f6f2df1b35a
reginakd <162291667+reginakd@users.noreply.github.com>
2024-03-09 23:07:35 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,81 +1,93 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+sns.set(style='dark')
 
-# memuat dataset
-main_data = pd.read_csv("https://raw.githubusercontent.com/reginakd/proyek_analisis_data/main/dashboard/main_data.csv")
+def create_sum_sharing(df):
+    sumshare_df = df.resample(rule='D', on='dteday').agg({
+        "casual": "sum",
+        "registered": "sum",
+        "cnt": "sum",
+        "windspeed": "mean"  
+    })
+    sumshare_df = sumshare_df.reset_index()
+    return sumshare_df
 
-def create_hum(df):
-    hum_df = df.groupby(by="hum").agg({
+def create_monthly_sharing(df):
+    monthlyshare_df = df.resample(rule='M', on='dteday').agg({
         "casual": "sum",
         "registered": "sum",
         "cnt": "sum"
-    }).reset_index()  
-    return hum_df
+    })
+    monthlyshare_df.index = monthlyshare_df.index.strftime('%B %Y')
+    monthlyshare_df = monthlyshare_df.reset_index()
+    return monthlyshare_df
 
-def create_temp(df):
-    temp_df = df.groupby(by="temp").agg({
+def create_yearly_sharing(df):
+    yearlyshare_df = df.resample(rule='Y', on='dteday').agg({
         "casual": "sum",
         "registered": "sum",
-        "cnt": "sum"
-    }).reset_index()  
-    return temp_df
+        "cnt": "sum",
+        "windspeed": "mean"  
+    })
+    yearlyshare_df.index = yearlyshare_df.index.strftime('%Y')
+    yearlyshare_df.reset_index(inplace=True)  
+    return yearlyshare_df
 
-def create_windspeed(df):
-    windspeed_df = df.groupby(by="windspeed").agg({
-        "casual": "sum",
-        "registered": "sum",
-        "cnt": "sum"
-    }).reset_index()  
-    return windspeed_df
+clean_df = pd.read_csv("https://raw.githubusercontent.com/reginakd/proyek_analisis_data/main/dashboard/main_data.csv")
+clean_df["dteday"] = pd.to_datetime(clean_df["dteday"])
 
-# memilih opsi bulan
-selected_month = st.sidebar.selectbox("Select Month", range(1, 13), 1)
+min_date = clean_df["dteday"].min()
+max_date = clean_df["dteday"].max()
 
-# filter data berdasarkan bulan yang dipilih
-filtered_data = main_data[main_data['mnth'] == selected_month]
+with st.sidebar:
+    st.subheader("Data Visualization")
+    start_date, end_date = st.date_input(
+        label='Time span',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
 
-# judul dataset
-st.title("Bike Sharing Data Dashboard")
+main_df = clean_df[(clean_df["dteday"] >= str(start_date)) &
+                   (clean_df["dteday"] <= str(end_date))]
 
-# menampilkan dataset
-st.write("## Bike Sharing Data for Month :sparkles:", selected_month)
-st.write(filtered_data)
+sumsharing_df = create_sum_sharing(main_df)
+yearlysum_df = create_yearly_sharing(main_df)
+monthlysum_df = create_yearly_sharing(main_df)
 
-# Visualisasi data
-st.write("## Data Visualizations")
+st.header("## Bike Sharing Data for Month :sparkles:")
 
-# grafik untuk jumlah rental berdasarkan hari dalam sebulan
-plt.figure(figsize=(10, 6))
-sns.barplot(data=filtered_data, x='dteday', y='cnt', ci=None)
-plt.title("Total Rental Count by Day")
-plt.xticks(rotation=90)
-fig, ax = plt.subplots()
-ax.bar(filtered_data['dteday'], filtered_data['cnt'])
-ax.tick_params(axis='x', labelsize=5)  # 
-plt.xticks(rotation=90)
+# Grafik jumlah penyewaan sepeda dalam setahun (registered + casual)
+st.subheader("Total of Bike Sharing per Year")
+fig, ax = plt.subplots(figsize=(10, 6))
+x = np.arange(len(yearlysum_df))
+ax.plot(x, yearlysum_df['cnt'], marker='o', label='Total', color='pink')
+ax.plot(x, yearlysum_df['casual'] + yearlysum_df['registered'], marker='o', label='Casual + Registered', color='blue')
+ax.set_xticks(x)
+ax.set_xticklabels(yearlysum_df['dteday'], rotation=60, ha='right')
+ax.legend(loc='upper center')
+ax.set_ylabel('Total Bike Sharing', font='10')
+ax.set_xlabel('Year', font='10')
+plt.tight_layout()
 st.pyplot(fig)
 
-# grafik untuk suhu dan perasaan suhu berdasarkan hari dalam sebulan
-plt.figure(figsize=(10, 6))
-sns.boxplot(data=filtered_data, x='dteday', y='temp', color='skyblue', width=0.4, linewidth=1)
-sns.boxplot(data=filtered_data, x='dteday', y='atemp', color='orange', width=0.4, linewidth=1)
-plt.title("Temperature and Feeling Temperature by Day")
-plt.xticks(rotation=45)
-plt.legend(labels=['Temperature', 'Feeling Temperature'])
-fig, ax = plt.subplots()
-ax.boxplot([filtered_data['temp'], filtered_data['atemp']], positions=[1, 2])
-ax.set_xticklabels(['Temperature', 'Feeling Temperature'])
-plt.xticks(rotation=45)
+# Grafik penyewaan sepeda setiap bulan
+st.subheader("Total of Bike Sharing per Month")
+fig, ax = plt.subplots(figsize=(16, 8))
+x = monthlysum_df['dteday']
+ax.plot(x, monthlysum_df['casual'], marker='o', color='green')
+ax.plot(x, monthlysum_df['registered'], marker='o', color='purple')
+ax.tick_params(axis='x', rotation=90)
+ax.legend(["Casual", "Registered"])
 st.pyplot(fig)
 
-# grafik untuk kelembaban dan kecepatan angin
-plt.figure(figsize=(10, 6))
-sns.scatterplot(data=filtered_data, x='hum', y='windspeed', hue='weathersit', palette='viridis', alpha=0.7)
-plt.title("Humidity vs Wind Speed")
-fig, ax = plt.subplots()
-ax.scatter(filtered_data['hum'], filtered_data['windspeed'])
-plt.title("Humidity vs Wind Speed")
+# Grafik pengaruh kelembapan terhadap jumlah penyewa sepeda
+st.subheader("The Effect of Humidity on Bicycle Sharing per Month")
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(main_df['hum'], main_df['cnt'], color='brown', alpha=0.5)
+ax.set_xlabel('Hum in %', font='10')
+ax.set_ylabel('Total Bike Sharing', font='10')
+plt.tight_layout()
 st.pyplot(fig)
-
